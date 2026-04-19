8966046da5c8521c36521bf7ca5392b1f6ea57aa
kingman-sa <kingman@gmail.com>
2026-04-19 11:25:02 +0200

    primeiro commit backend mpesa

---
+++ server.js
@@ -0,0 +1,47 @@
+require("dotenv").config();
+const express = require("express");
+const axios = require("axios");
+
+const app = express();
+app.use(express.json());
+
+app.post("/pay", async (req, res) => {
+    try {
+        const { amount, phone } = req.body;
+
+        if (!amount || !phone) {
+            return res.status(400).json({
+                error: "amount e phone são obrigatórios"
+            });
+        }
+
+        const response = await axios({
+            method: "post",
+            url: "https://boldpay-thxc.onrender.com/api/v1/payment/c2b",
+            headers: {
+                "Accept": "application/json",
+                "Content-Type": "application/json",
+                "apikey": process.env.API_KEY
+            },
+            data: {
+                amount: amount,
+                to: process.env.TO,
+                from: phone,
+                reference: process.env.REFERENCE,
+                transaction: Date.now().toString(),
+                subject: "Bewise"
+            }
+        });
+
+        res.json(response.data);
+
+    } catch (err) {
+        res.status(500).json({
+            error: err.response?.data || err.message
+        });
+    }
+});
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log("Rodando na porta " + (process.env.PORT || 3000));
+});
